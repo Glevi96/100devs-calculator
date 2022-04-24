a50e5933dfbd2f3e63f8f5087ddfa33efbeb2658
glevi96 <gulyas.levente96@hotmail.com>
2022-04-24 13:24:23 +0200

    made the buttons interactive

--- js/main.js
+++ js/main.js
@@ -1,9 +1,58 @@
-document.querySelector("#seven").addEventListener("click", clickEvent);
-function clickEvent(){
+document.querySelector("#one").addEventListener("click", oneEvent);
+document.querySelector("#two").addEventListener("click", twoEvent);
+document.querySelector("#three").addEventListener("click", threeEvent);
+document.querySelector("#four").addEventListener("click", fourEvent);
+document.querySelector("#five").addEventListener("click", fiveEvent);
+document.querySelector("#six").addEventListener("click", sixEvent);
+document.querySelector("#seven").addEventListener("click", sevenEvent);
+document.querySelector("#eight").addEventListener("click", eightEvent);
+document.querySelector("#nine").addEventListener("click", nineEvent);
+document.querySelector("#zero").addEventListener("click", zeroEvent);
+document.querySelector("#calc").addEventListener("click",calcEvent)
+function oneEvent(){
+    let value = document.querySelector("#one").innerHTML;
+    document.querySelector("#displayMonitor").innerHTML += value;
+}
+function twoEvent(){
+    let value = document.querySelector("#two").innerHTML;
+    document.querySelector("#displayMonitor").innerHTML += value;
+}
+function threeEvent(){
+    let value = document.querySelector("#three").innerHTML;
+    document.querySelector("#displayMonitor").innerHTML += value;
+}
+function fourEvent(){
+    let value = document.querySelector("#four").innerHTML;
+    document.querySelector("#displayMonitor").innerHTML += value;
+}
+function fiveEvent(){
+    let value = document.querySelector("#five").innerHTML;
+    document.querySelector("#displayMonitor").innerHTML += value;
+}
+function sixEvent(){
+    let value = document.querySelector("#six").innerHTML;
+    document.querySelector("#displayMonitor").innerHTML += value;
+}
+function sevenEvent(){
     let value = document.querySelector("#seven").innerHTML;
     document.querySelector("#displayMonitor").innerHTML += value;
 }
+function eightEvent(){
+    let value = document.querySelector("#eight").innerHTML;
+    document.querySelector("#displayMonitor").innerHTML += value;
+}
+function nineEvent(){
+    let value = document.querySelector("#nine").innerHTML;
+    document.querySelector("#displayMonitor").innerHTML += value;
+}
+function zeroEvent(){
+    let value = document.querySelector("#zero").innerHTML;
+    document.querySelector("#displayMonitor").innerHTML += value;
+}
+function calcEvent(){
+    let 
+}
 function Calculator(){
     let currentValue = 0;
-
+    let equation = "";
 }
