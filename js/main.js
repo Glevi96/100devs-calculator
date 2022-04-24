55ddad1cbf5aa04c827f5c327bc17a7015e31d15
glevi96 <gulyas.levente96@hotmail.com>
2022-04-24 19:44:30 +0200

    made all buttons interactive

--- js/main.js
+++ js/main.js
@@ -1,56 +1,60 @@
-document.querySelector("#one").addEventListener("click", oneEvent);
-document.querySelector("#two").addEventListener("click", twoEvent);
-document.querySelector("#three").addEventListener("click", threeEvent);
-document.querySelector("#four").addEventListener("click", fourEvent);
-document.querySelector("#five").addEventListener("click", fiveEvent);
-document.querySelector("#six").addEventListener("click", sixEvent);
-document.querySelector("#seven").addEventListener("click", sevenEvent);
-document.querySelector("#eight").addEventListener("click", eightEvent);
-document.querySelector("#nine").addEventListener("click", nineEvent);
-document.querySelector("#zero").addEventListener("click", zeroEvent);
-document.querySelector("#calc").addEventListener("click",calcEvent)
-function oneEvent(){
-    let value = document.querySelector("#one").innerHTML;
+let equlat = [];
+document.querySelector("#one").addEventListener("click", () =>{
+    clickEvent("#one");
+});
+document.querySelector("#two").addEventListener("click", () =>{
+    clickEvent("#two");
+});
+document.querySelector("#three").addEventListener("click", () =>{
+    clickEvent("#three");
+});
+document.querySelector("#four").addEventListener("click", () =>{
+    clickEvent("#four");
+});
+document.querySelector("#five").addEventListener("click", () =>{
+    clickEvent("#five");
+});
+document.querySelector("#six").addEventListener("click", () =>{
+    clickEvent("#six");
+});
+document.querySelector("#seven").addEventListener("click", () =>{
+    clickEvent("#seven");
+});
+document.querySelector("#eight").addEventListener("click", () =>{
+    clickEvent("#eight");
+});
+document.querySelector("#nine").addEventListener("click", () =>{
+    clickEvent("#nine");
+});
+document.querySelector("#zero").addEventListener("click", () =>{
+    clickEvent("#zero");
+});
+document.querySelector("#division").addEventListener("click", () =>{
+    clickEvent("#division");
+});
+document.querySelector("#multiply").addEventListener("click", () =>{
+    clickEvent("#multiply");
+});
+document.querySelector("#add").addEventListener("click", () =>{
+    clickEvent("#add");
+});
+document.querySelector("#sub").addEventListener("click", () =>{
+    clickEvent("#sub");
+});
+document.querySelector("#decimal").addEventListener("click", () =>{
+    clickEvent("#decimal");
+});
+document.querySelector("#calculate").addEventListener("click",calcEvent)
+
+function clickEvent(s1){
+    let value = document.querySelector(s1).innerHTML;
     document.querySelector("#displayMonitor").innerHTML += value;
-}
-function twoEvent(){
-    let value = document.querySelector("#two").innerHTML;
-    document.querySelector("#displayMonitor").innerHTML += value;
-}
-function threeEvent(){
-    let value = document.querySelector("#three").innerHTML;
-    document.querySelector("#displayMonitor").innerHTML += value;
-}
-function fourEvent(){
-    let value = document.querySelector("#four").innerHTML;
-    document.querySelector("#displayMonitor").innerHTML += value;
-}
-function fiveEvent(){
-    let value = document.querySelector("#five").innerHTML;
-    document.querySelector("#displayMonitor").innerHTML += value;
-}
-function sixEvent(){
-    let value = document.querySelector("#six").innerHTML;
-    document.querySelector("#displayMonitor").innerHTML += value;
-}
-function sevenEvent(){
-    let value = document.querySelector("#seven").innerHTML;
-    document.querySelector("#displayMonitor").innerHTML += value;
-}
-function eightEvent(){
-    let value = document.querySelector("#eight").innerHTML;
-    document.querySelector("#displayMonitor").innerHTML += value;
-}
-function nineEvent(){
-    let value = document.querySelector("#nine").innerHTML;
-    document.querySelector("#displayMonitor").innerHTML += value;
-}
-function zeroEvent(){
-    let value = document.querySelector("#zero").innerHTML;
-    document.querySelector("#displayMonitor").innerHTML += value;
+    equlat.push(value);
 }
 function calcEvent(){
-    let 
+    let equ = document.querySelector("#displayMonitor").innerHTML;
+    console.log(equ);
+    console.log(equlat);
 }
 function Calculator(){
     let currentValue = 0;
